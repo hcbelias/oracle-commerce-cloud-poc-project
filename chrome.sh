fc65fc4125c9a475e9001413182642092fa6bd18
Henrique elias <hcbe2004@gmail.com>
2018-06-20 11:01:56 -0300

    including certificates and ccproxy.sh

--- chrome.sh
+++ chrome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
- "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --proxy-server="http://localhost:8088" --user-data-dir="~/.chrome-occ" https://ccadmin-zaja.oracleoutsourcing.com/occs-admin/
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --proxy-server="http://localhost:8088" --user-data-dir="~/.chrome-occ" https://ccadmin-zaja.oracleoutsourcing.com/occs-admin/
