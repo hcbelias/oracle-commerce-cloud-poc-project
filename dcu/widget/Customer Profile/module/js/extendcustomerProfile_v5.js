ce60804091d57aeb0afd04170db0280f0740fa76
henrique elias <hcbe2004@gmail.com>
2018-07-12 20:40:28 -0300

    commit account changes - sidenav

---
+++ dcu/widget/Customer Profile/module/js/extendcustomerProfile_v5.js
@@ -0,0 +1,29 @@
+/**
+ * @fileoverview extendcustomerProfile_v5.js.
+ *
+ * @author 
+ */
+define(
+  //---------------------
+  // DEPENDENCIES
+  //---------------------
+  [],
+
+  //-----------------------
+  // MODULE DEFINITION
+  //-----------------------
+  function () {
+ 
+    "use strict";
+    return {
+ 
+      onLoad: function(widget) {
+        //console.log('extendcustomerProfile_v5.js onLoad');
+      }, 
+      
+      beforeAppear: function() {
+        //console.log('extendcustomerProfile_v5.js before appear');
+      }
+    };
+  }
+);
