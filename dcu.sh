ff36d04d272141248d30a6aa17969a9e360899d9
Henrique Elias <hcbe2004@gmail.com>
2018-07-14 11:58:23 -0300

    changes to use application key update profile view

--- dcu.sh
+++ dcu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z $1 ]; then
-	echo "./dcu.sh [pass] [extra options]"
+	echo "./dcu.sh [extra options]"
 	exit 1
 fi 
 
-dcu -n https://ccadmin-zaja.oracleoutsourcing.com -u helias@avenuecode.com -b . -p $*
+dcu -n https://ccadmin-zaja.oracleoutsourcing.com -k eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI3ZDE2ZjFiMC1kYWJiLTQ1ZDItYmYxMC1jM2E5M2RkOTI2ZGMiLCJpc3MiOiJhcHBsaWNhdGlvbkF1dGgiLCJleHAiOjE1NjMxMTM3NjgsImlhdCI6MTUzMTU3Nzc2OH0=.VkjXSd4gm4bS/wPJOiN7aGCh42w5zlq4gdkRRWPlWrI= -b .
